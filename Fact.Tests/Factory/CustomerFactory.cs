5f19740ec9c90bdfdea8a3f326f27d5dc8488c35
yan de lima justino <yan_limaj@hotmail.com>
2015-06-21 15:41:37 -0300

    removing specs feactures

--- Fact.Tests/Factory/CustomerFactory.cs
+++ Fact.Tests/Factory/CustomerFactory.cs
@@ -7,25 +7,25 @@ namespace Fact.Tests.Factory
     {
         public static void Build()
         {
-            Builder.Current.Set(new Customer
+            var customer = new Customer
             {
                 Id = "build-test-01",
                 Address = Faker.BR.AddressBR.Address,
                 Email = Faker.Internet.Email("customer"),
                 Name = Faker.Name.FullName,
                 Site = Faker.Internet.HttpUri
-            });
+            };
 
+            var customer_without_email = new Customer
+            {
+                Id = "build-test-02",
+                Address = Faker.BR.AddressBR.Address,
+                Name = Faker.Name.FullName,
+                Site = Faker.Internet.HttpUri
+            };
 
-
-            Builder.Current.Set("without email",
-                new Customer
-                {
-                    Id = "build-test-02",
-                    Address = Faker.BR.AddressBR.Address,
-                    Name = Faker.Name.FullName,
-                    Site = Faker.Internet.HttpUri
-                });
+            Builder.Current.Set(customer);
+            Builder.Current.Set("without email", customer_without_email);
         }
     }
 }
