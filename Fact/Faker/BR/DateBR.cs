0bbb97f35d19894ea7f71d4c8784010fa7f7583f
Eduardo Baltazar <eduardo.baltazar@hotmail.com>
2015-06-23 12:05:04 -0300

    Set public to AddressUS, add class DateAndTime and add class DateBR

---
+++ Fact/Faker/BR/DateBR.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Fact.Faker.BR
+{
+    public class DateBR : DateAndTime
+    {
+        public static string Extenso
+        {
+            get
+            {
+                var dateTime = Date;
+                var culture = new CultureInfo("pt-BR");
+                var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+
+                return $"{ dayOfWeek }, { dateTime.Day } de { culture.DateTimeFormat.GetMonthName(dateTime.Month) } de { dateTime.Year }";
+            }
+        }
+    }
+}
